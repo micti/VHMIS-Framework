c8bc5555474ad842893c945798351c93624b5bac
Le Nhat Anh <lenhatanh86@gmail.com>
2012-02-24 15:12:56 +0700

    Hoàn thiện class Vhmis_Date cơ bản. Close #1

--- Core/Date.php
+++ Core/Date.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Class xử lý ngày tháng
+ *
+ * Chú ý : hiện tại chỉ hổ trợ năm 1900 đến năm 2038 (unix timestamp 32bit)
+ */
+
 class Vhmis_Date
 {
     /**
@@ -10,8 +16,183 @@ class Vhmis_Date
         @date_default_timezone_set($zone);
     }
 
+    /**
+     * Chênh lệch thời gian của hệ thống với GMT
+     */
+    protected $_offsetServer;
+
+    /**
+     * Giờ đang được thiết lập của đối tượng (ứng với timezone của hệ thống) dạng unix timestamp
+     */
+    protected $_time;
+
+    /**
+     * Chênh lệch thời gian của giờ đang được thiệt lập với GMT
+     */
+    protected $_offset;
+
+    /**
+     * Khởi tạo
+     */
     public function __construct()
     {
+        $this->_offsetServer = date("Z");
+        $this->time();
+    }
 
+    /**
+     * Thiết lập thời gian
+     *
+     * Nếu param là mảng thì param có dạng
+     * 'd'
+     * 'm'
+     * 'y'
+     * 'h'
+     * 'm'
+     * 's'
+     *
+     * @param mixed $param Thông tin ngày giờ khởi tạo (dạng array,string),  còn nếu null (mặc định) thì trả về ngày giờ hiện tại
+     * @param mixed $offset Chênh lệch theo giờ với UTC, Nếu là null thì offset = offset của server
+     */
+    public function time($param = null, $offset = null)
+    {
+        if(is_array($param))
+        {
+            foreach($param as $i => $j)
+            {
+                $$i = intval($j);
+            }
+            $time = mktime($h, $m, $s, $m, $d, $y);
+        }
+        else if(is_string($param))
+        {
+            $time = strtotime($param);
+        }
+        else if($param === null)
+        {
+            $time = time();
+        }
+
+        if($time === false) return false;
+
+        $this->_time = $time;
+
+        if($offset != null && is_numeric($offset))
+        {
+            $this->_offset = $offset * 3600 - $this->_offsetServer;
+            $this->_time -= $this->_offset;
+        }
+        else
+        {
+            $this->_offset = $this->_offsetServer;
+        }
+    }
+
+    /**
+     * Xuất ra thời gian đã qua của thời gian ở đối tượng so với thời gian hiện tại
+     * 3 phút trước, 2 giờ 3 phút trước ....
+     * Kết quả trả về mạng tính tương đối (vì một năm = 365 ngày, 1 = 30 ngày)
+     *
+     * @param int $deep Số lượng tối đa đại lương thời gian cần thông báo
+     */
+    public function toAgo($deep = 2)
+    {
+        $now = time();
+        if($now < $this->_time)
+        {
+            //function $this->toWait();
+            return;
+        }
+
+        $pass = $now - $this->_time;
+
+        if($pass == 0) return 'vừa mới';
+
+        // Tính số các đại lượng năm, tháng, tuần, ngày, giờ, phút, giây
+        $year = floor($pass / 31536000);
+        $pass -= $year * 31536000;
+        $month = floor($pass / 2592000);
+        $pass -= $month * 2592000;
+        $week = floor($pass / 604800);
+        $pass -= $week * 604800;
+        $day = floor($pass / 86400);
+        $pass -= $day * 86400;
+        $hour = floor($pass / 3600);
+        $pass -= $hour * 3600;
+        $min = floor($pass / 60);
+        $pass -= $min * 60;
+        $sec = $pass;
+
+        // Chuỗi kết quả
+        $string = '';
+
+        // Lấy chuỗi
+        if($deep != 0 && $year != 0) {
+            $string .= $year . ' năm ';
+            $deep--;
+            if($deep == 0) return $string . 'trước';
+        }
+
+        if($deep != 0 && $month != 0) {
+            $string .= $month . ' tháng ';
+            $deep--;
+            if($deep == 0) return $string . 'trước';
+        }
+
+        if($deep != 0 && $week != 0) {
+            $string .= $week . ' tuần ';
+            $deep--;
+            if($deep == 0) return $string . 'trước';
+        }
+
+        if($deep != 0 && $day != 0) {
+            $string .= $day . ' ngày ';
+            $deep--;
+            if($deep == 0) return $string . 'trước';
+        }
+
+        if($deep != 0 && $hour != 0) {
+            $string .= $hour . ' giờ ';
+            $deep--;
+            if($deep == 0) return $string . 'trước';
+        }
+
+        if($deep != 0 && $min != 0) {
+            $string .= $min . ' phút ';
+            $deep--;
+            if($deep == 0) return $string . 'trước';
+        }
+
+        if($deep != 0 && $sec != 0) {
+            $string .= $sec . ' giây ';
+            $deep--;
+            if($deep == 0) return $string . 'trước';
+        }
+
+        return $string . 'trước';
+    }
+
+    /**
+     * Xuất ra dạng dd-mm-yyyy
+     */
+    public function __toString()
+    {
+        return date('d-m-Y', $this->_time);
+    }
+
+    /**
+     * Xuất ra để lưu cơ sở dữ liệu
+     */
+    public function toSQL($full = true)
+    {
+        return $full ? date('Y-m-d H:i:s', $this->_time) : date('Y-m-d', $this->_time);
+    }
+
+    /**
+     * Xuất ra dành cho RSS
+     */
+    public function toRSS()
+    {
+        //
     }
 }
---
+++ Test/Date.php
@@ -0,0 +1,76 @@
+<?php
+
+// Cảnh báo toàn bộ
+error_reporting(E_ALL | E_NOTICE);
+
+// PHP 5.1 trở lên hổ trợ thời gian từ 1901 đến 2038 (32 bit t_time on system)
+$a = mktime(0, 0, 0, 1, 1, 1902);
+$b = mktime(23, 59, 59, 12, 31, 2037);
+$c = mktime(0, 0, 0, 1, 1, 1970);
+$d = pow(2, 31);
+var_dump($a);
+var_dump($c);
+var_dump($b);
+var_dump($d);
+
+echo '<br>';
+
+@date_default_timezone_set("ASIA/HO_CHI_MINH");
+
+$c = mktime(0, 0, 0, 1, 1, 1970);
+$d = gmmktime(0, 0, 0, 1, 1, 1970);
+
+var_dump($c);
+var_dump($d);
+var_dump(($c - $d) / 3600);
+
+echo '<br>';
+
+var_dump(date("e", time()));
+var_dump(date("I", time()));
+var_dump(date("O", time()));
+var_dump(date("P", time()));
+var_dump(date("T", time()));
+var_dump(date("Z", time()));
+
+echo '<br>';
+
+var_dump(date('l jS \of F Y h:i:s A', strtotime('2012-02-03 08:26:06 +07:00')));
+var_dump(date('l jS \of F Y h:i:s A', strtotime('2012-02-02 08:26:06 +08:00')));
+var_dump(date('l jS \of F Y h:i:s A', strtotime('2012-03-02 08:26:06 +07:00')));
+
+require '../Core/Date.php';
+require '../Core/Benchmark.php';
+
+$mark = new Vhmis_Benchmark();
+
+$mark->timer('a');
+$date = new Vhmis_Date();
+$mark->timer('b');
+echo $mark->time('a', 'b') . '<br />';
+
+echo '<br>';
+echo $date . '|';
+echo $date->toSQL() . '|';
+echo $date->toSQL(false) . '|';
+echo var_dump($date);
+
+$mark->timer('c');
+echo '<br />' . $mark->time('a', 'c') . '<br />';
+
+$date->time('+1 day', 8);
+echo '<br>';
+echo $date . '|';
+echo $date->toSQL() . '|';
+echo $date->toSQL(false) . '|';
+echo var_dump($date);
+
+$mark->timer('d');
+echo '<br />' . '<br />' . $mark->time('a', 'd') . '<br />';
+
+$date->time('-1 day -3 hour');
+echo '<br>';
+echo $date->toAgo();
+
+$mark->timer('e');
+echo '<br />' . '<br />' . $mark->time('a', 'e') . '<br />';
