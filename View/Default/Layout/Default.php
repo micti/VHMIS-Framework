3c842ab007e8d48cdcffeffd0a095c314654ca3c
Le Nhat Anh <lenhatanh86@gmail.com>
2012-03-08 11:17:32 +0700

    Hổ trợ nhiều kích thước layout

--- View/Default/Layout/Default.php
+++ View/Default/Layout/Default.php
@@ -42,7 +42,7 @@ if(isset($jsFiles))
     </head>
     <body>
         <header>
-            <div class="container global_bar">
+            <div class="container<?php if(isset($layoutSize)) echo ' container-' . $layoutSize;?> global_bar">
                 <ul class="nav pull-left">
 <?php
 foreach($config['apps']['list']['url'] as $appurl)
@@ -57,7 +57,7 @@ foreach($config['apps']['list']['url'] as $appurl)
                     <li><a href="<?php echo $config['site']['path'] . $config['apps']['logout-url']; ?>">Thoát</a></li>
                 </ul>
             </div>
-            <div class="container app_header">
+            <div class="container<?php if(isset($layoutSize)) echo ' container-' . $layoutSize;?> app_header">
                 <h1 class="app-title-header app-title-header-<?php echo $appInfo['url']; ?>"><?php echo $appInfo['app']; ?></h1>
 <?php
 if(isset($_appMenu) && is_array($_appMenu))
@@ -76,7 +76,7 @@ if(isset($_appMenu) && is_array($_appMenu))
             echo '">' . "\n";
             foreach($menu[2] as $submenu)
             {
-                echo '                                <li><a href="' .  $config['site']['fullpath'] . $submenu[1] . '">' . $submenu[0] . '</a></li>' . "\n";
+                echo '                                <li><a href="' . $config['site']['fullpath'] . $submenu[1] . '">' . $submenu[0] . '</a></li>' . "\n";
             }
             echo '                            </ul>'. "\n";
         }
@@ -89,7 +89,7 @@ if(isset($_appMenu) && is_array($_appMenu))
 ?>
             </div>
             <div id="page_header">
-                <div class="container page_header_bar">
+                <div class="container<?php if(isset($layoutSize)) echo ' container-' . $layoutSize;?> page_header_bar">
                     <ul class="breadcrumb">
                         <li><a href="<?php echo $config['site']['path']; ?>">Trang chủ</a> <span class="divider">/</span></li>
                         <li><a href="<?php echo $config['site']['fullpath']; ?>"><?php echo $appInfo['app']; ?></a> <span class="divider">/</span></li>
@@ -123,13 +123,13 @@ if(isset($pageTitle) && $pageTitle != '') {
             </div>
         </header>
         <div id="page_container">
-            <div class="container">
+            <div class="container<?php if(isset($layoutSize)) echo ' container-' . $layoutSize;?>">
                 <?php echo $content; ?>
 
             </div>
         </div>
         <footer>
-            <div class="container footer">
+            <div class="container<?php if(isset($layoutSize)) echo ' container-' . $layoutSize;?> footer">
                 <p>
                     Giao diện "Default" là giao diện mặc định được sử dụng trong quá trình phát triển hệ thống VHMIS &copy; Trung Tâm CNTT - Trường CĐ CNTT Hữu Nghị Việt Hàn 2011<br />
                     Sử dụng HTML5, CSS3 và Javascript, tương thích với các trình duyệt hiện đại như <a href="#">Chrome, Firefox, Safari</a>
