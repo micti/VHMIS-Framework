5e14d12a17ca0102d306b2844a475caefd5c31bb
Le Nhat Anh <lenhatanh86@gmail.com>
2012-03-15 14:23:02 +0700

    Override phương thức tìm theo primary key
    Nếu không có kết quả thì trả về null, thay vì đối tượng Rowset với 0 dòng

--- Core/Model.php
+++ Core/Model.php
@@ -33,4 +33,39 @@ abstract class Vhmis_Model extends Zend_Db_Table_Abstract
     {
         return $this->toArray($object);
     }
+
+    /**
+     * Hàm lấy dữ liệu theo primary key
+     *
+     * @param mixed $keys Giá trị của các primary key
+     */
+    public function find($keys)
+    {
+        // Đối với bảng chỉ có 1 primary key, có thể đưa vào chuỗi chứa nhiều primary key, cách nhau bằng dấu phẩy
+        if(is_string($keys))
+        {
+            $keys = explode(',', $keys);
+        }
+
+        $rowset = parent::find($keys);
+
+        $total = count($rowset);
+
+        if($total > 1) return $rowset;
+
+        if($total == 1) return $rowset[0];
+
+        return null;
+    }
+
+    /**
+     * Lấy dữ liệu theo primary key id
+     * Hàm này là ánh xạ của hàm find()
+     *
+     * @param mixed $ids Giá trị của các id
+     */
+    public function getById($ids)
+    {
+        return $this->find($ids);
+    }
 }
