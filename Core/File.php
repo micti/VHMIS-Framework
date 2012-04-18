f93f6da4c1356435641f1ed23e8d876408978561
Le Nhat Anh <lenhatanh86@gmail.com>
2012-04-18 14:43:06 +0700

    Thêm Core Class File
    Xử lý file

---
+++ Core/File.php
@@ -0,0 +1,22 @@
+<?php
+
+class Vhmis_File
+{
+    protected $_filename;
+    protected $_directory;
+
+    public function __construct($path, $create = false, $mode = '0755')
+    {
+
+    }
+
+    public static function delete($file)
+    {
+        return @unlink($file);
+    }
+
+    public static function move($oldFile, $newFile)
+    {
+        return @rename($oldFile, $newFile);
+    }
+}
