076eed088594ff9e26825252c43a2067243bd56c
Le Nhat Anh <lenhatanh86@gmail.com>
2012-04-26 15:18:35 +0700

    Chèn cứng nocache

--- Core/Network/Response.php
+++ Core/Network/Response.php
@@ -32,6 +32,9 @@ class Vhmis_Network_Response
      */
     public function response()
     {
+        //header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+        //header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+
         $this->_sendContent();
     }
 
