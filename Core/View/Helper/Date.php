b21d1c5a471a250c46a2ea59c9ed4c9f1098ca51
Le Nhat Anh <lenhatanh86@gmail.com>
2012-05-08 08:57:03 +0700

    Thêm các hàm cho helper Date

--- Core/View/Helper/Date.php
+++ Core/View/Helper/Date.php
@@ -2,6 +2,8 @@
 
 class Vhmis_View_Helper_Date
 {
+    private $_vietnameseWeekday = array('Chủ nhật', 'Thứ 2', 'Thứ 3', 'Thứ 4', 'Thứ 5', 'Thứ 6', 'Thứ 7');
+
     /**
      * Chuyển đổi định dạng giờ sql sang định dạng thường khác
      *
@@ -10,5 +12,18 @@ class Vhmis_View_Helper_Date
     public function sqlToNormal($time, $format = 'd/m/Y')
     {
         return Vhmis_Date::sqlToNormal($time, $format);
+    }
+
+    /**
+     * Chuyển đổi định dạng giờ sql sang định dạng đầy đủ chữ VN
+     *
+     * @param time Giờ dạng sql
+     */
+    public function sqlToFullVietnameseText($time)
+    {
+        $text1 = $this->sqlToNormal($time, 'w');
+        $text2 = $this->sqlToNormal($time, 'd/m/Y h:i:s');
+
+        return $this->_vietnameseWeekday[$text1] . ' ngày ' . $text2;
     }
 }
