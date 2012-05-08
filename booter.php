f6f3b5075e0a5f4170ca1d38a063b9179c437321
Le Nhat Anh <lenhatanh86@gmail.com>
2012-05-08 08:56:15 +0700

    Class Security :) dựa trên CI

--- booter.php
+++ booter.php
@@ -264,6 +264,39 @@ function ___ctv($string)
 }
 
 /**
+ * Remove Invisible Characters, hàm từ CI
+ *
+ * This prevents sandwiching null characters
+ * between ascii characters, like Java\0script.
+ *
+ * @param	string
+ * @param	bool
+ * @return	string
+ */
+function ___removeInvisibleCharacters($str, $url_encoded = TRUE)
+{
+	$non_displayables = array();
+
+	// every control character except newline (dec 10),
+	// carriage return (dec 13) and horizontal tab (dec 09)
+	if ($url_encoded)
+	{
+		$non_displayables[] = '/%0[0-8bcef]/';	// url encoded 00-08, 11, 12, 14, 15
+		$non_displayables[] = '/%1[0-9a-f]/';	// url encoded 16-31
+	}
+
+	$non_displayables[] = '/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]+/S';	// 00-08, 11, 12, 14-31, 127
+
+	do
+	{
+		$str = preg_replace($non_displayables, '', $str, -1, $count);
+	}
+	while ($count);
+
+	return $str;
+}
+
+/**
  * Hàm bỏ ký tự \ cho dữ liệu mảng một hay nhiều chiều
  *
  * @param array $values mảng cần thực hiện việc loại bỏ
