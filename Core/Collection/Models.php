557bc8c63d993c57c3fca315daf342a802619f3d
Le Nhat Anh <lenhatanh86@gmail.com>
2012-10-26 08:04:06 +0700

    Format

--- Core/Collection/Models.php
+++ Core/Collection/Models.php
@@ -2,19 +2,24 @@
 
 class Vhmis_Collection_Models extends Vhmis_Collection_Objects
 {
+
+    /**
+     *
+     * @param string $name Tên model
+     * @param array|null $param Thông số khai báo
+     * @return Vhmis_Model Đối tượng Vhmis_Model được gọi
+     */
     public function load($name, $param = null)
     {
         $class = 'Vhmis_Model_' . $name;
         $name = ___ctv($name);
-        if($param == null)
-        {
+        if ($param == null) {
             $this->_loaded[$name] = new $class();
-        }
-        else
-        {
+        } else {
             $this->_loaded[$name] = new $class($param);
         }
 
         return $this->_loaded[$name];
     }
+
 }
