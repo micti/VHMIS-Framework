895f82942f784ce294e869c3e666b5cb8d2f6777
Le Nhat Anh <lenhatanh86@gmail.com>
2013-01-24 08:18:18 +0700

    Tạo lớp Config để đọc các file config

--- CoreVer2/Config/Config.php
+++ CoreVer2/Config/Config.php
@@ -12,13 +12,86 @@
 namespace Vhmis\Config;
 
 /**
- * Class dùng để tạo các đối tượng config
+ * Class dùng để lấy các config của hệ thống
  *
  * @category   Vhmis
  * @package    Vhmis_Config
- * @subpackage Config
  */
 class Config
 {
-    //put your code here
+
+    /**
+     * Load config của hệ thống ứng dụng
+     *
+     * @param string $name
+     * @param string $part Phần muồn lấy
+     * @return array
+     */
+    public static function system($name, $part = '')
+    {
+        $config = Load::filePhp(VHMIS_SYS_CONF_PATH . '/' . $name . '.php');
+
+        if ($config === null || empty($config)) {
+            echo 'No System Config : ' . $name;
+            exit();
+        }
+
+        if (is_string($part) && $part != '') {
+            $parts = explode('/', $part);
+            foreach ($parts as $p) {
+                if (isset($config[$p])) {
+                    $config = $config[$p];
+                } else {
+                    echo 'No Part : ' . $part . ' at App Config : ' . $app;
+                    exit();
+                }
+            }
+        }
+
+        return $config;
+    }
+
+    /**
+     * Load config của 1 ứng dụng
+     *
+     * @param string $app
+     * @param string $part Phần muồn lấy
+     * @param array
+     */
+    public static function app($app, $part = '')
+    {
+        $config = Load::filePhp(VHMIS_APPS_PATH . D_SPEC . ___fUpper($app)
+                        . D_SPEC . 'Config' . D_SPEC . 'App.php');
+
+        if ($config === null || empty($config)) {
+            echo 'No App Config : ' . $app;
+            exit();
+        }
+
+        if (is_string($part) && $part != '') {
+            $part = strtolower($part);
+            $parts = explode('/', $part);
+            foreach ($parts as $p) {
+                if (isset($config[$p])) {
+                    $config = $config[$p];
+                } else {
+                    echo 'No Part : ' . $part . ' at App Config : ' . $app;
+                    exit();
+                }
+            }
+        }
+
+        return $config;
+    }
+
+    /**
+     * Load config route của 1 ứng dụng
+     *
+     * @param string $app
+     * @param array
+     */
+    public static function appRoutes($app)
+    {
+        return self::app($app, 'Routes');
+    }
 }
