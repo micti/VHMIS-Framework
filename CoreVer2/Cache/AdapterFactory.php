1ce5279d9ff74e31189290891cd1bd378873abd8
Le Nhat Anh <lenhatanh86@gmail.com>
2013-04-04 16:13:52 +0700

    Thêm AdapterFactory cho Cache

---
+++ CoreVer2/Cache/AdapterFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Vhmis\Cache;
+
+abstract class AdapterFactory
+{
+    /**
+     *
+     * @param string $adapter
+     * @param type $config
+     * @return \Vhmis\Cache\Adapter\StorageInterface
+     */
+    public static function fatory($adapter, $config)
+    {
+        $class = 'Vhmis\\Cache\\Adapter\\' . $adapter;
+        $adapter = new $class($config);
+
+        return $adapter;
+    }
+}
--- Test/Cache.php
+++ Test/Cache.php
@@ -8,15 +8,17 @@ define('D_SPEC', DIRECTORY_SEPARATOR);
 require '../CoreVer2/Cache/Adapter/StorageInterface.php';
 require '../CoreVer2/Cache/Adapter/Memcached.php';
 require '../CoreVer2/Cache/Adapter/File.php';
+require '../CoreVer2/Cache/AdapterFactory.php';
 
-$cache = new \Vhmis\Cache\Adapter\Memcached(array('persistent' => 'akaka'));
+$cache = \Vhmis\Cache\AdapterFactory::fatory('Memcached', array('persistent' => 'akaka'));
+
 $cache->addServer();
 
 $cache->set('a', 1);
 
 echo $cache->get('a');
 
-$cache2 = new \Vhmis\Cache\Adapter\File(array('path' => '/WebServer/www/t2j/Cache/Data'));
+$cache2 = \Vhmis\Cache\AdapterFactory::fatory('File', array('path' => '/WebServer/www/t2j/Cache/Data'));
 
 $cache2->set('a', 1);
 
