2aee234759e4df1fb582425971388d0132cc6d79
Le Nhat Anh <lenhatanh86@gmail.com>
2013-06-08 16:08:44 +0700

    Component

---
+++ Vhmis/Component/Component.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Vhmis\Component;
+
+use \Vhmis\Di\ServiceManagerAwareInterface;
+use \Vhmis\Di\ServiceManager;
+
+/**
+ * Component
+ */
+class Component implements ServiceManagerAwareInterface
+{
+    /**
+     *
+     * @var \Vhmis\Di\ServiceManager
+     */
+    protected $sm;
+
+    /**
+     * Thiết lập Service Manager
+     * 
+     * @param \Vhmis\Di\ServiceManager $sm
+     */
+    public function setServiceManager(ServiceManager $sm) {
+        $this->sm = $sm;
+    }
+}
