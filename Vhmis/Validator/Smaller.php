6631c0afdaaefcc7038bb2465c970e7912ea5635
Le Nhat Anh <lenhatanh86@gmail.com>
2013-08-06 18:52:57 +0700

    Add Smaller and NotEqual validators

---
+++ Vhmis/Validator/Smaller.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Vhmis\Validator;
+
+class Smaller extends ValidatorAbstract
+{
+    /**
+     * Giá trị được đem ra so sánh
+     *
+     * @var mixed
+     */
+    protected $comparedValue;
+
+    public function setOptions($options)
+    {
+        if(isset($options['compare'])) {
+            $this->comparedValue = $options['compare'];
+        }
+
+        return $this;
+    }
+
+    /**
+     * Kiểm tra xem có nhỏ hơn với giá trị cần so sánh không
+     *
+     * @param mixed $value
+     * @return boolean
+     */
+    public function isValid($value)
+    {
+        $this->value = $value;
+        $this->standardValue = null;
+
+        if ($value > $this->comparedValue) {
+            return false;
+        }
+
+        $this->standardValue = $value;
+        return true;
+    }
+}
