4cd3184f62d5f5210fc7e13a635dfacdc52d7769
Le Nhat Anh <lenhatanh86@gmail.com>
2013-08-14 14:19:38 +0700

    Fix function name

--- Vhmis/Network/Request.php
+++ Vhmis/Network/Request.php
@@ -233,7 +233,7 @@ class Request
      */
     public function isAjaxRequest()
     {
-        if (issset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') {
+        if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') {
             return true;
         }
 
