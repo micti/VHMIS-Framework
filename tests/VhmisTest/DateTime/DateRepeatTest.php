823140a12481afe0656eff3ebb59f52d6707a7ec
Le Nhat Anh <lenhatanh86@gmail.com>
2014-05-30 15:18:16 +0700

    More tests

--- tests/VhmisTest/DateTime/DateRepeatTest.php
+++ tests/VhmisTest/DateTime/DateRepeatTest.php
@@ -28,10 +28,41 @@ class DateRepeatTest extends \PHPUnit_Framework_TestCase
     public function testGetRepeat()
     {
         $this->assertInstanceOf('Vhmis\DateTime\DateRepeat\AbstractRepeat', $this->repeat->getRepeat());
+        $rule = $this->repeat->getRule();
+        $rule->setRepeatByDay();
+        $this->assertInstanceOf('Vhmis\DateTime\DateRepeat\Day', $this->repeat->getRepeat());
+        $rule->setRepeatByWeek();
+        $this->assertInstanceOf('Vhmis\DateTime\DateRepeat\Week', $this->repeat->getRepeat());
+        $rule->setRepeatByMonth();
+        $this->assertInstanceOf('Vhmis\DateTime\DateRepeat\Month', $this->repeat->getRepeat());
+        $rule->setRepeatByYear();
+        $this->assertInstanceOf('Vhmis\DateTime\DateRepeat\Year', $this->repeat->getRepeat());
+    }
+
+    public function testEndDate()
+    {
+        $this->assertEquals('2100-12-31', $this->repeat->endDate());
+    }
+
+    public function testRepeatedDates()
+    {
+        $this->assertEquals(array(), $this->repeat->repeatedDates('2014-01-01', '2015-02-02'));
     }
 
     public function testGetRule()
     {
         $this->assertInstanceOf('Vhmis\DateTime\DateRepeat\Rule', $this->repeat->getRule());
+    }
+
+    public function testSetStartDateOfWeek()
+    {
+        $repeat = new DateRepeat;
+        $repeat->setStartDayOfWeek(4);
+        $repeat->getRule()->setRepeatByWeek()->setBaseDate('2014-05-28')->setRepeatWeekdays(array(3, 5))
+            ->setRepeatTimes(2)->setFrequency(2);
+        $this->assertEquals('2014-06-06', $repeat->endDate());
+
+        $repeat->setStartDayOfWeek(1);
+        $this->assertEquals('2014-05-30', $repeat->endDate());
     }
 }
