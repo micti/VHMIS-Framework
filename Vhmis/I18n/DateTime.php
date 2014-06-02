a96bfdd3a29088d1c827e4f746508414443e7989
Le Nhat Anh <lenhatanh86@gmail.com>
2014-06-02 11:15:39 +0700

    DateTime class for I18n

---
+++ Vhmis/I18n/DateTime.php
@@ -0,0 +1,192 @@
+<?php
+
+/**
+ * Vhmis Framework
+ *
+ * @link http://github.com/micti/VHMIS-Framework for git source repository
+ * @copyright Le Nhat Anh (http://lenhatanh.com)
+ * @license http://opensource.org/licenses/MIT MIT License
+ */
+
+namespace Vhmis\I18n;
+
+use \IntlCalendar;
+use \IntlDateFormatter;
+use \Locale;
+
+/**
+ * DateTime
+ */
+class DateTime
+{
+    /**
+     * IntlCalendar object
+     *
+     * @var IntlCalendar
+     */
+    protected $calendar;
+
+    public function __construct($timezone = null, $calendar = null, $locale = null)
+    {
+        if($timezone === null) {
+            $timezone = date_default_timezone_get();
+        }
+
+        if($calendar === null) {
+            $calendar = 'gregorian';
+        }
+
+        if($locale === null) {
+            $locale = Locale::getDefault();
+        }
+        
+        $calendarLocale = $locale . '@calendar=' . $calendar;
+        $this->calendar = IntlCalendar::createInstance($timezone, $calendarLocale);
+
+        if($this->calendar === null) {
+            return null;
+        }
+    }
+
+    public function set($time) {
+        if(!($time instanceof \DateTime)) {
+            $time = new \DateTime($time);
+        }
+
+        if($time === false) {
+            return $this;
+        }
+
+        $mili = $time->getTimestamp() * 1000;
+        $this->calendar->setTime($mili);
+
+        return $this;
+    }
+
+    public function formatISODate() {
+        return IntlDateFormatter::formatObject($this->calendar, 'yyyy-MM-dd');
+    }
+
+    public function formatISODateTime() {
+        return IntlDateFormatter::formatObject($this->calendar, 'yyyy-MM-dd HH:mm:ss');
+    }
+
+    /**
+     *
+     * @param string $format
+     * 
+     * @return string
+     */
+    public function format($format) {
+        $formatMap = array(
+            'd' => 'dd',
+            'Y' => 'yyyy',
+            'm' => 'MM',
+            'h' => 'HH',
+            'i' => 'mm',
+            's' => 'ss'
+        );
+
+        if(isset($formatMap[$format])) {
+            return IntlDateFormatter::formatObject($this->calendar, $formatMap[$format]);
+        }
+
+        return '';
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getDay() {
+        return $this->format('d');
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getMonth() {
+        return $this->format('d');
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getYear() {
+        return $this->format('Y');
+    }
+
+    /**
+     *
+     * @param int $amount
+     * 
+     * @return \Vhmis\I18n\DateTime
+     */
+    public function addSecond($amount) {
+        $this->calendar->add(IntlCalendar::FIELD_SECOND, $amount);
+
+        return $this;
+    }
+
+    /**
+     *
+     * @param int $amount
+     *
+     * @return \Vhmis\I18n\DateTime
+     */
+    public function addMinute($amount) {
+        $this->calendar->add(IntlCalendar::FIELD_MINUTE, $amount);
+
+        return $this;
+    }
+
+    /**
+     *
+     * @param int $amount
+     *
+     * @return \Vhmis\I18n\DateTime
+     */
+    public function addHour($amount) {
+        $this->calendar->add(IntlCalendar::FIELD_HOUR, $amount);
+
+        return $this;
+    }
+
+    /**
+     *
+     * @param int $amount
+     *
+     * @return \Vhmis\I18n\DateTime
+     */
+    public function addDay($amount) {
+        $this->calendar->add(IntlCalendar::FIELD_DAY_OF_MONTH, $amount);
+
+        return $this;
+    }
+
+    /**
+     *
+     * @param int $amount
+     *
+     * @return \Vhmis\I18n\DateTime
+     */
+    public function addMonth($amount) {
+        $this->calendar->add(IntlCalendar::FIELD_MONTH, $amount);
+
+        return $this;
+    }
+
+    /**
+     *
+     * @param int $amount
+     *
+     * @return \Vhmis\I18n\DateTime
+     */
+    public function addYear($amount) {
+        $this->calendar->add(IntlCalendar::FIELD_YEAR, $amount);
+
+        return $this;
+    }
+}
---
+++ tests/VhmisTest/I18n/DateTimeTest.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Vhmis Framework
+ *
+ * @link http://github.com/micti/VHMIS-Framework for git source repository
+ * @copyright Le Nhat Anh (http://lenhatanh.com)
+ * @license http://opensource.org/licenses/MIT MIT License
+ */
+
+namespace VhmisTest\I18n;
+
+use Vhmis\I18n\DateTime;
+
+class DateTimeTest extends \PHPUnit_Framework_TestCase
+{
+    protected $date;
+
+    public function setUp()
+    {
+        $this->date = new DateTime('GMT+07:00');
+    }
+
+    public function testFormatISO()
+    {
+        $this->date->set('2014-05-06');
+        $this->assertEquals('2014-05-06', $this->date->formatISODate());
+
+        $this->date->set('2014-05-06 00:12:34');
+        $this->assertEquals('2014-05-06 00:12:34', $this->date->formatISODateTime());
+
+        $this->date->set('2014-05-31 00:12:34');
+        $this->assertEquals('2014-05-31 00:12:34', $this->date->formatISODateTime());
+    }
+
+    public function testAddSecond()
+    {
+        $this->date->set('2014-01-31 00:12:34')->addSecond(31);
+        $this->assertEquals('2014-01-31 00:13:05', $this->date->formatISODateTime());
+    }
+
+    public function testAddMinute()
+    {
+        $this->date->set('2014-01-31 00:12:34')->addMinute(60);
+        $this->assertEquals('2014-01-31 01:12:34', $this->date->formatISODateTime());
+    }
+
+    public function testAddHour()
+    {
+        $this->date->set('2014-01-31 00:12:34')->addHour(25);
+        $this->assertEquals('2014-02-01 01:12:34', $this->date->formatISODateTime());
+    }
+
+    public function testAddDay()
+    {
+        $this->date->set('2014-01-31')->addDay(31);
+        $this->assertEquals('2014-03-03', $this->date->formatISODate());
+        $this->date->addDay(365);
+        $this->assertEquals('2015-03-03', $this->date->formatISODate());
+    }
+
+    public function testAddMonth()
+    {
+        $this->date->set('2014-01-31')->addMonth(1);
+        $this->assertEquals('2014-02-28', $this->date->formatISODate());
+        $this->date->addMonth(1);
+        $this->assertEquals('2014-03-28', $this->date->formatISODate());
+    }
+
+    public function testAddYear()
+    {
+        $this->date->set('2012-02-29')->addYear(3);
+        $this->assertEquals('2015-02-28', $this->date->formatISODate());
+        $this->date->addYear(1);
+        $this->assertEquals('2016-02-28', $this->date->formatISODate());
+    }
+}
