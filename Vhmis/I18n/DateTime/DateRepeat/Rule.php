2da50728dae1e7dc01ced7c8b652cbfd5789cffe
Le Nhat Anh <lenhatanh86@gmail.com>
2014-07-27 15:42:41 +0700

    Repeat rule

---
+++ Vhmis/I18n/DateTime/DateRepeat/Rule.php
@@ -0,0 +1,532 @@
+<?php
+
+namespace Vhmis\I18n\DateTime\DateRepeat;
+
+use \Vhmis\I18n\DateTime\DateTime;
+use \Vhmis\Utils\Exception\InvalidArgumentException;
+
+class Rule
+{
+    /**
+     * Repeate by, 4-7 for day, week, month, year
+     * Default is 4 (repeat by day)
+     *
+     * @var int
+     */
+    protected $by = 4;
+    protected $baseDate;
+    protected $baseDay;
+    protected $baseWeekday;
+    protected $baseMonth;
+    protected $endDate;
+
+    /**
+     * Times of repeat (including base date)
+     * Default is 0 (no end)
+     *
+     * @var int
+     */
+    protected $times = 0;
+
+    /**
+     * Frequency of repeat
+     * Default is 1
+     *
+     * @var int
+     */
+    protected $freq = 1;
+
+    /**
+     * Type of repeated date in month
+     *
+     * @var string
+     */
+    protected $type = 'day';
+
+    /**
+     *
+     * @var int[]
+     */
+    protected $repeatedWeekdays = array();
+
+    /**
+     *
+     * @var int
+     */
+    protected $repeatedDay;
+
+    /**
+     *
+     * @var int
+     */
+    protected $repeatedDayPosition;
+
+    /**
+     *
+     * @var int[]
+     */
+    protected $repeatedDays = array();
+
+    /**
+     *
+     * @var int[]
+     */
+    protected $repeatedMonths = array();
+
+    /**
+     * DateTime helper object
+     *
+     * @var DateTime
+     */
+    protected $date;
+
+    /**
+     * Contruct
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Set repeat by day/week/month/year
+     *
+     * @param int $by
+     *
+     * @return Rule
+     *
+     * @throws InvalidArgumentException
+     */
+    public function setRepeatBy($by)
+    {
+        $this->by = $this->fixInt($by, 4, 7);
+
+        return $this;
+    }
+
+    /**
+     * Set base date
+     *
+     * @param DateTime $date
+     *
+     * @return Rule
+     *
+     * @throws InvalidArgumentException
+     */
+    public function setBaseDate($date)
+    {
+        if ($date instanceof DateTime === false) {
+            throw new InvalidArgumentException('Date is not valid');
+        }
+
+        $this->baseDate = $date->getDateWithExtendedYear();
+
+        $this->date = $date;
+        $this->baseWeekday = $this->date->getField(7);
+        $this->baseDay = $this->date->getField(5);
+        $this->baseMonth = $this->date->getField(2);
+
+        // auto
+        $this->repeatedDay = $this->baseWeekday;
+        $this->repeatedDayPosition = ceil($this->baseDay / 7) - 1;
+        $this->repeatedDays = array($this->baseDay);
+        $this->repeatedMonths = array($this->baseMonth);
+        $this->repeatedWeekdays = array($this->baseWeekday);
+
+        return $this;
+    }
+
+    /**
+     *
+     * @param string $date
+     *
+     * @return Rule
+     *
+     * @throws InvalidArgumentException
+     */
+    public function setEndDate($date)
+    {
+        if ($this->date == null) {
+            $dateObject = new DateTime;
+            $dateObject->modify($date);
+            $dateEnd = $dateObject->getDateWithExtendedYear();
+        } else {
+            $this->date->modify($date);
+            $dateEnd = $this->date->getDateWithExtendedYear();
+            $this->date->modify($this->baseDate);
+        }
+
+        if ($dateEnd !== $date) {
+            throw new InvalidArgumentException('End date is not valid');
+        }
+
+        $this->endDate = $date;
+
+        return $this;
+    }
+
+    /**
+     * Set times of repeat
+     *
+     * @param int $times
+     *
+     * @return Rule
+     *
+     * @throws InvalidArgumentException
+     */
+    public function setRepeatTimes($times)
+    {
+        $this->times = (int) $times;
+
+        if ($this->times < 0) {
+            throw new InvalidArgumentException('Must be int and greater than -1');
+        }
+
+        return $this;
+    }
+
+    /**
+     * Set frequency
+     *
+     * @param int $freq
+     *
+     * @return Rule
+     *
+     * @throws InvalidArgumentException
+     */
+    public function setFrequency($freq)
+    {
+        $this->freq = (int) $freq;
+
+        if ($this->freq < 1) {
+            throw new InvalidArgumentException('Must be int and greater than 0');
+        }
+
+        return $this;
+    }
+
+    /**
+     * Set repeated weekdays
+     * Use 1-7 for sunday to staturday
+     * Accept int array or int string spec by ','
+     *
+     * @param int[]|string $weekdays
+     *
+     * @return Rule
+     *
+     * @throws InvalidArgumentException
+     */
+    public function setRepeatWeekdays($weekdays)
+    {
+        $this->repeatedWeekdays = $this->fixIntArray($weekdays, 1, 7);
+
+        return $this;
+    }
+
+    /**
+     * Set type of date repeat
+     *
+     * - Repeat by day in month: 2nd, 3rd ....
+     * - Repeat by relative day in month: first Monday, second Tuesday, last day ...
+     */
+    public function setType($type)
+    {
+        $this->type = 'day';
+
+        if ($type === 'relative_day') {
+            $this->type = 'relative_day';
+        }
+
+        return $this;
+    }
+
+    /**
+     * Set day of repeated day in month
+     * 0 common day, 1 - 7 for sunday to saturday, 8 working day, 9 weekdend
+     *
+     * @param string|int $day
+     *
+     * @return Rule
+     *
+     * @throws InvalidArgumentException
+     */
+    public function setRepeatedDay($day)
+    {
+        $this->repeatedDay = $this->fixInt($day, 0, 9);
+
+        return $this;
+    }
+
+    /**
+     * Set position of day of repeated day in month
+     * (for type = relative_day)
+     *
+     * @param string|int position
+     *
+     * @return Rule
+     *
+     * @throws InvalidArgumentException
+     */
+    public function setRepeatedDayPosition($position)
+    {
+        // -10 - 10 ????
+        $this->repeatedDayPosition = $this->fixInt($position, -10, 10);
+
+        return $this;
+    }
+
+    /**
+     * Set repeated days, use 1-31, accept int array or int string spec by ','
+     * (for type = month)
+     *
+     * @param int[]|string $days
+     *
+     * @return \Vhmis\DateTime\DateRepeat\Week
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function setRepeatedDays($days)
+    {
+        $min = 1;
+        $max = 31;
+
+        if ($this->date !== null) {
+            $maxInfo = $this->date->getMaximumValueOfField(5);
+            $max = $maxInfo['greatest'];
+            $minInfo = $this->date->getMinimumValueOfField(5);
+            $min = $minInfo['least'];
+        }
+
+        $this->repeatedDays = $this->fixIntArray($days, $min, $max);
+
+        return $this;
+    }
+
+    /**
+     * Set repeated months, use 1-12, accept int array or int string spec by ','
+     *
+     * @param int[]|string $months
+     *
+     * @return \Vhmis\DateTime\DateRepeat\Week
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function setRepeatedMonths($months)
+    {
+        $min = 1;
+        $max = 12;
+
+        if ($this->date !== null) {
+            $maxInfo = $this->date->getMaximumValueOfField(2);
+            $max = $maxInfo['greatest'];
+            $minInfo = $this->date->getMinimumValueOfField(2);
+            $min = $minInfo['least'];
+        }
+
+        $this->repeatedMonths = $this->fixIntArray($months, $min, $max);
+
+        return $this;
+    }
+
+    /**
+     * Get valid of rule
+     *
+     * @return boolean
+     */
+    public function isValid()
+    {
+        if ($this->baseDate === null) {
+            return false;
+        }
+
+        switch ($this->by) {
+            case 5:
+                return $this->isValidRepeatByWeek();
+            case 6:
+                return $this->isValidRepeatByMonth();
+            case 7:
+                return $this->isValidRepeatByYear();
+            default:
+                return true; // case 4
+        }
+    }
+
+    /**
+     * Get value of rule
+     *
+     * @return array
+     */
+    public function getInfo()
+    {
+        return array(
+            'by'          => $this->by,
+            'base'        => $this->baseDate,
+            'baseDay'     => $this->baseDay,
+            'baseWeekday' => $this->baseWeekday,
+            'baseMonth'   => $this->baseMonth,
+            'end'         => $this->endDate,
+            'times'       => $this->times,
+            'freq'        => $this->freq,
+            'type'        => $this->type,
+            'days'        => $this->repeatedDays,
+            'weekdays'    => $this->repeatedWeekdays,
+            'months'      => $this->repeatedMonths,
+            'day'         => $this->repeatedDay,
+            'position'    => $this->repeatedDayPosition
+        );
+    }
+
+    /**
+     * Reset rule
+     *
+     * @return \Vhmis\DateTime\DateRepeat\Rule
+     */
+    public function reset()
+    {
+        $this->baseDate = $this->endDate = null;
+        $this->baseDay = $this->baseMonth = $this->baseWeekday = null;
+        $this->repeatedDay = $this->repeatedDayPosition = null;
+        $this->by = 4;
+        $this->times = 0;
+        $this->freq = 1;
+        $this->type = 'day';
+        $this->repeatedMonths = $this->repeatedWeekdays = $this->repeatedDays = array();
+
+        return $this;
+    }
+
+    /**
+     * Check valid of repeat by week
+     *
+     * @return boolean
+     */
+    protected function isValidRepeatByWeek()
+    {
+        if (array_search($this->baseWeekday, $this->repeatedWeekdays) === false) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Check valid of repeat by month
+     *
+     * @return boolean
+     */
+    protected function isValidRepeatByMonth()
+    {
+        if ($this->type === 'day') {
+            return $this->isValidDay();
+        }
+
+        return $this->isValidRelativeDay();
+    }
+
+    /**
+     * Check valid of repeat by year
+     *
+     * @return boolean
+     */
+    protected function isValidRepeatByYear()
+    {
+        if (array_search($this->baseMonth, $this->repeatedMonths) === false) {
+            return false;
+        }
+
+        if ($this->type === 'day') {
+            return true;
+        }
+
+        return $this->isValidRelativeDay();
+    }
+
+    /**
+     * Check day
+     *
+     * @return boolean
+     */
+    protected function isValidDay()
+    {
+        if (array_search($this->baseDay, $this->repeatedDays) === false) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Check relative day
+     *
+     * @return boolean
+     */
+    protected function isValidRelativeDay()
+    {
+        $date = clone $this->date;
+        $date->gotoFirstDayOfMonth();
+
+        if ($date->getDateWithExtendedYear() === $this->baseDate) {
+            $date->gotoLastDayOfMonth();
+        }
+
+        $date->gotoNthDayOfMonth($this->repeatedDay, $this->repeatedDayPosition);
+        $currentDate = $date->getDateWithExtendedYear();
+
+        if ($this->baseDate !== $currentDate) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Check and fix int value
+     *
+     * @param int $number
+     * @param int $min
+     * @param int $max
+     *
+     * @return int
+     *
+     * @throws InvalidArgumentException
+     */
+    protected function fixInt($number, $min, $max)
+    {
+        $number = (int) $number;
+        if ($number < $min || $number > $max) {
+            throw new InvalidArgumentException('Only int from ' . $min . ' - ' . $max);
+        }
+
+        return $number;
+    }
+
+    /**
+     * Check and fix int array
+     *
+     * @param int[]|string $data
+     * @param int          $min
+     * @param int          $max
+     *
+     * @return int[]
+     *
+     * @throws InvalidArgumentException
+     */
+    protected function fixIntArray($data, $min, $max)
+    {
+        $data = is_string($data) ? explode(',', $data) : $data;
+
+        if (!is_array($data)) {
+            throw new InvalidArgumentException('Only int array or string spec by `,`. From ' . $min . ' - ' . $max);
+        }
+
+        // Check
+        foreach ($data as &$number) {
+            $number = $this->fixInt($number, $min, $max);
+        }
+
+        $data = array_unique($data);
+        sort($data);
+
+        return $data;
+    }
+}
---
+++ tests/VhmisTest/I18n/DateTime/DateRepeat/RuleTest.php
@@ -0,0 +1,440 @@
+<?php
+
+/**
+ * Vhmis Framework
+ *
+ * @link http://github.com/micti/VHMIS-Framework for git source repository
+ * @copyright Le Nhat Anh (http://lenhatanh.com)
+ * @license http://opensource.org/licenses/MIT MIT License
+ */
+
+namespace VhmisTest\I18n\DateTime\DateRepeat;
+
+use \Vhmis\I18n\DateTime\DateTime;
+use \Vhmis\I18n\DateTime\DateRepeat\Rule;
+
+class RuleTest extends \PHPUnit_Framework_TestCase
+{
+    protected $rule;
+
+    public function setUp()
+    {
+        $this->rule = new Rule;
+    }
+
+    /**
+     * @expectedException \Vhmis\Utils\Exception\InvalidArgumentException
+     */
+    public function testSetByException()
+    {
+        $this->rule->setRepeatBy(3);
+    }
+
+    /**
+     * @expectedException \Vhmis\Utils\Exception\InvalidArgumentException
+     */
+    public function testSetByException2()
+    {
+        $this->rule->setRepeatBy(8);
+    }
+
+    public function testSetBy()
+    {
+        $this->rule->setRepeatBy(7);
+        $info = $this->rule->getInfo();
+
+        $this->assertEquals(7, $info['by']);
+    }
+
+    /**
+     * @expectedException \Vhmis\Utils\Exception\InvalidArgumentException
+     */
+    public function testSetBaseDateException()
+    {
+        $this->rule->setBaseDate(3);
+    }
+
+    public function testSetBaseDate()
+    {
+        $date = new DateTime('Asia/Ho_Chi_Minh');
+        $date->setDate(2014, 7, 22);
+
+        $this->rule->setBaseDate($date);
+        $info = $this->rule->getInfo();
+
+        $this->assertEquals('2014-07-22', $info['base']);
+        $this->assertEquals(22, $info['baseDay']);
+        $this->assertEquals(3, $info['baseWeekday']);
+        $this->assertEquals(7, $info['baseMonth']);
+        $this->assertEquals(array(22), $info['days']);
+        $this->assertEquals(array(3), $info['weekdays']);
+        $this->assertEquals(array(7), $info['months']);
+    }
+
+    /**
+     * @expectedException \Vhmis\Utils\Exception\InvalidArgumentException
+     */
+    public function testSetEndDateException()
+    {
+        $this->rule->setEndDate('fdfd');
+    }
+
+    public function testSetEndDate()
+    {
+        $this->rule->setEndDate('2123-12-12');
+
+        $info = $this->rule->getInfo();
+
+        $this->assertEquals('2123-12-12', $info['end']);
+    }
+
+    /**
+     * @expectedException \Vhmis\Utils\Exception\InvalidArgumentException
+     */
+    public function testSetRepeatTimesException()
+    {
+        $this->rule->setRepeatTimes(-4);
+    }
+
+    public function testSetRepeatTimes()
+    {
+        $this->rule->setRepeatTimes(8);
+
+        $info = $this->rule->getInfo();
+
+        $this->assertEquals(8, $info['times']);
+    }
+
+    /**
+     * @expectedException \Vhmis\Utils\Exception\InvalidArgumentException
+     */
+    public function testSetFrequencyException()
+    {
+        $this->rule->setFrequency(0);
+    }
+
+    public function testSetFrequency()
+    {
+        $this->rule->setFrequency(3);
+
+        $info = $this->rule->getInfo();
+
+        $this->assertEquals(3, $info['freq']);
+    }
+
+    /**
+     * @expectedException \Vhmis\Utils\Exception\InvalidArgumentException
+     */
+    public function testSetRepeatWeekdaysException()
+    {
+        $this->rule->setRepeatWeekdays(2);
+    }
+
+    /**
+     * @expectedException \Vhmis\Utils\Exception\InvalidArgumentException
+     */
+    public function testSetRepeatWeekdaysException2()
+    {
+        $this->rule->setRepeatWeekdays('a');
+    }
+
+    /**
+     * @expectedException \Vhmis\Utils\Exception\InvalidArgumentException
+     */
+    public function testSetRepeatWeekdaysException3()
+    {
+        $this->rule->setRepeatWeekdays('0,1,2,3,4');
+    }
+
+    public function testSetRepeatWeekdays()
+    {
+        $this->rule->setRepeatWeekdays(array(3,5,6,6));
+
+        $info = $this->rule->getInfo();
+
+        $this->assertEquals(array(3,5,6), $info['weekdays']);
+    }
+
+    public function testSetTypeDay()
+    {
+        $this->rule->setType('day');
+
+        $info = $this->rule->getInfo();
+
+        $this->assertEquals('day', $info['type']);
+    }
+
+    public function testSetTypeRelativeDay()
+    {
+        $this->rule->setType('relative_day');
+
+        $info = $this->rule->getInfo();
+
+        $this->assertEquals('relative_day', $info['type']);
+    }
+
+    /**
+     * @expectedException \Vhmis\Utils\Exception\InvalidArgumentException
+     */
+    public function testSetRepeatDayException()
+    {
+        $this->rule->setRepeatedDay('-1');
+    }
+
+    /**
+     * @expectedException \Vhmis\Utils\Exception\InvalidArgumentException
+     */
+    public function testSetRepeatDayException2()
+    {
+        $this->rule->setRepeatedDay(10);
+    }
+
+    public function testSetRepeatDay()
+    {
+        $this->rule->setRepeatedDay(2);
+
+        $info = $this->rule->getInfo();
+
+        $this->assertEquals(2, $info['day']);
+    }
+
+    /**
+     * @expectedException \Vhmis\Utils\Exception\InvalidArgumentException
+     */
+    public function testSetRepeatedDayPositionException()
+    {
+        $this->rule->setRepeatedDayPosition('11');
+    }
+
+    /**
+     * @expectedException \Vhmis\Utils\Exception\InvalidArgumentException
+     */
+    public function testSetRepeatedDayPositionException2()
+    {
+        $this->rule->setRepeatedDayPosition(-11);
+    }
+
+    public function testSetRepeatedDayPosition()
+    {
+        $this->rule->setRepeatedDayPosition('2');
+
+        $info = $this->rule->getInfo();
+
+        $this->assertEquals(2, $info['position']);
+    }
+
+    /**
+     * @expectedException \Vhmis\Utils\Exception\InvalidArgumentException
+     */
+    public function testSetRepeatedDaysException()
+    {
+        $this->rule->setRepeatedDays('33');
+    }
+
+    /**
+     * @expectedException \Vhmis\Utils\Exception\InvalidArgumentException
+     */
+    public function testSetRepeatedDaysException2()
+    {
+        $this->rule->setRepeatedDays(array(2,-1));
+    }
+
+    public function testSetRepeatedDays()
+    {
+        $this->rule->setRepeatedDays('2,4,5,19,12');
+
+        $info = $this->rule->getInfo();
+
+        $this->assertEquals(array(2,4,5,12,19), $info['days']);
+    }
+
+    /**
+     * @expectedException \Vhmis\Utils\Exception\InvalidArgumentException
+     */
+    public function testSetRepeatedMonthException()
+    {
+        $this->rule->setRepeatedMonths(array(14));
+    }
+
+    /**
+     * @expectedException \Vhmis\Utils\Exception\InvalidArgumentException
+     */
+    public function testSetRepeatedMonthsException2()
+    {
+        $this->rule->setRepeatedMonths(array(5,7,-11));
+    }
+
+    public function testSetRepeatedMonths()
+    {
+        $this->rule->setRepeatedDays('2,4,5,19,12');
+
+        $info = $this->rule->getInfo();
+
+        $this->assertEquals(array(2,4,5,12,19), $info['days']);
+    }
+
+    public function testReset()
+    {
+        $result = array(
+            'by'          => 4,
+            'base'        => null,
+            'baseDay'     => null,
+            'baseWeekday' => null,
+            'baseMonth'   => null,
+            'end'         => null,
+            'times'       => 0,
+            'freq'        => 1,
+            'type'        => 'day',
+            'days'        => array(),
+            'weekdays'    => array(),
+            'months'      => array(),
+            'day'         => null,
+            'position'    => null
+        );
+
+        $this->assertEquals($result, $this->rule->reset()->getInfo());
+    }
+
+    public function testIsValidNotBaseDate()
+    {
+        $this->rule->reset();
+
+        $this->assertEquals(false, $this->rule->isValid());
+    }
+
+    public function testIsValidForDay()
+    {
+        $this->rule->reset();
+
+        $date = new DateTime;
+        $date->setDateWithExtenedYear(2014, 7, 22);
+
+        $this->rule->setBaseDate($date);
+
+        $this->assertEquals(true, $this->rule->isValid());
+    }
+
+    public function testIsValidForWeek()
+    {
+        $this->rule->reset();
+
+        $date = new DateTime;
+        $date->setDateWithExtenedYear(2014, 7, 22);
+
+        $this->rule->setBaseDate($date);
+        $this->rule->setRepeatBy(5);
+        $this->rule->setRepeatWeekdays('4,5');
+
+        $this->assertEquals(false, $this->rule->isValid());
+    }
+
+    public function testIsValidForWeek2()
+    {
+        $this->rule->reset();
+
+        $date = new DateTime;
+        $date->setDateWithExtenedYear(2014, 7, 22);
+
+        $this->rule->setBaseDate($date);
+        $this->rule->setRepeatBy(5);
+        $this->rule->setRepeatWeekdays('3,5');
+
+        $this->assertEquals(true, $this->rule->isValid());
+    }
+
+    public function testIsValidForMonth2()
+    {
+        $this->preTest(6);
+
+        $this->rule->setType('day');
+        $this->rule->setRepeatedDays(array(12));
+
+        $this->assertEquals(false, $this->rule->isValid());
+    }
+
+    public function testIsValidForMonth3()
+    {
+        $this->preTest(6);
+
+        $this->rule->setType('day');
+        $this->rule->setRepeatedDays(array(12,22));
+
+        $this->assertEquals(true, $this->rule->isValid());
+    }
+
+    public function testIsValidForMonth4()
+    {
+        $this->preTest(6);
+
+        $this->rule->setType('relative_day');
+        $this->rule->setRepeatedDay(2);
+        $this->rule->setRepeatedDayPosition(3);
+
+        $this->assertEquals(false, $this->rule->isValid());
+    }
+
+    public function testIsValidForMonth5()
+    {
+        $this->preTest(6);
+
+        $this->rule->setType('relative_day');
+        $this->rule->setRepeatedDay(3);
+        $this->rule->setRepeatedDayPosition(4);
+
+        $this->assertEquals(true, $this->rule->isValid());
+    }
+
+    public function testIsValidForYear()
+    {
+        $this->preTest(7);
+
+        $this->rule->setRepeatedMonths(array(1));
+
+        $this->assertEquals(false, $this->rule->isValid());
+    }
+
+    public function testIsValidForYear2()
+    {
+        $this->preTest(7);
+
+        $this->rule->setRepeatedMonths(array(7,4));
+        $this->rule->setType('day');
+
+        $this->assertEquals(true, $this->rule->isValid());
+    }
+
+    public function testIsValidForYear3()
+    {
+        $this->preTest(7);
+
+        $this->rule->setRepeatedMonths(array(7,4));
+        $this->rule->setType('relative_day');
+        $this->rule->setRepeatedDay(3);
+        $this->rule->setRepeatedDayPosition(5);
+
+        $this->assertEquals(false, $this->rule->isValid());
+    }
+
+    public function testIsValidForYear4()
+    {
+        $this->preTest(7);
+
+        $this->rule->setRepeatedMonths(array(7,4));
+        $this->rule->setType('relative_day');
+        $this->rule->setRepeatedDay(3);
+        $this->rule->setRepeatedDayPosition(4);
+
+        $this->assertEquals(true, $this->rule->isValid());
+    }
+
+    public function preTest($by)
+    {
+        $this->rule->reset();
+
+        $date = new DateTime;
+        $date->setDateWithExtenedYear(2014, 7, 22);
+
+        $this->rule->setBaseDate($date);
+        $this->rule->setRepeatBy($by);
+    }
+}
