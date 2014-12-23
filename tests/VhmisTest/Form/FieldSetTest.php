9236c6692935248bcf59f782e3f36c7802b30ad7
Le Nhat Anh <lenhatanh86@gmail.com>
2014-12-23 08:32:37 +0700

    More test

--- tests/VhmisTest/Form/FieldSetTest.php
+++ tests/VhmisTest/Form/FieldSetTest.php
@@ -20,7 +20,7 @@ class FieldSetTest extends \PHPUnit_Framework_TestCase
         $this->assertSame($fac2, $fieldset->getFactory());
     }
 
-    public function testAddFieldAndFieldSet()
+    public function testAddField()
     {
         $fieldset = new FieldSet;
 
@@ -32,12 +32,34 @@ class FieldSetTest extends \PHPUnit_Framework_TestCase
 
         $fieldC = new Field;
         $this->assertFalse($fieldset->addField($fieldC));
+        
+        $this->assertFalse($fieldset->addField('a'));
+        $this->assertFalse($fieldset->addField(null));
+        $this->assertFalse($fieldset->addField(false));
 
         $allFields = $fieldset->getAllFields();
 
         $this->assertSame($fieldA, $allFields['a']);
         $this->assertInstanceOf('\Vhmis\Form\Field', $allFields['b']);
         $this->assertSame('b', $allFields['b']->getName());
+    }
+    
+    public function testAddFieldSet()
+    {
+        $fieldset = new FieldSet;
+
+        $fieldSetA = new FieldSet;
+        $fieldSetA->setName('a');
+        $fieldset->addFieldSet($fieldSetA);
+
+        $fieldset->addFieldSet(['name' => 'b']);
+
+        $fieldSetC = new FieldSet;
+        $this->assertFalse($fieldset->addFieldSet($fieldSetC));
+        
+        $this->assertFalse($fieldset->addFieldSet('a'));
+        $this->assertFalse($fieldset->addFieldSet(null));
+        $this->assertFalse($fieldset->addFieldSet(false));
     }
 
     public function testFillTest()
