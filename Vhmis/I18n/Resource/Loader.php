0cc35263ce98b7705e7cf0261fb5ab0c05b18c05
Le Nhat Anh <lenhatanh86@gmail.com>
2015-03-15 17:33:29 +0700

    Resource loader

---
+++ Vhmis/I18n/Resource/Loader.php
@@ -0,0 +1,196 @@
+<?php
+
+namespace Vhmis\I18n\Resource;
+
+use Vhmis\Utils\Exception\InvalidArgumentException;
+use Vhmis\Utils\Loader\PhpArray as LoaderData;
+
+class Loader
+{
+
+    /**
+     * Supported locales
+     *
+     * @var array
+     */
+    protected static $locales = [
+        'vi',
+        'vi-VN',
+        'en',
+        'en-US',
+        'ko',
+        'ko-KR'
+    ];
+
+    /**
+     * Supported main data
+     *
+     * @var array
+     */
+    protected static $mainData = [
+        'ca-buddhist',
+        'ca-chinese',
+        'ca-coptic',
+        'ca-dangi',
+        'ca-ethiopic-amete-alem',
+        'ca-ethiopic',
+        'ca-generic',
+        'ca-gregorian',
+        'ca-hebrew',
+        'ca-indian',
+        'ca-islamic-civil',
+        'ca-islamic-rgsa',
+        'ca-islamic-tbla',
+        'ca-islamic-umalqura',
+        'ca-islamic',
+        'ca-japanese',
+        'ca-persian',
+        'ca-roc',
+        'characters',
+        'currencies',
+        'dateFields',
+        'delimiters',
+        'languages',
+        'layout',
+        'listPatterns',
+        'localeDisplayNames',
+        'measurementSystemNames',
+        'numbers',
+        'posix',
+        'scripts',
+        'territories',
+        'timeZoneNames',
+        'units',
+        'variants',
+    ];
+
+    /**
+     * Supported supplemental data
+     *
+     * @var array
+     */
+    protected static $supplementalData = [
+        'plurals'
+    ];
+
+    /**
+     * Load supplemental data.
+     *
+     * @param string $field
+     * @param string $locale
+     *
+     * @return array
+     *
+     * @throws InvalidArgumentException
+     */
+    static public function loadSupplementalData($field)
+    {
+        if (!static::isSupportedSupplementalData($field)) {
+            throw new InvalidArgumentException('Data for ' . $field . ' not supported.');
+        }
+
+        $file = __DIR__ . D_SPEC . 'Data' . D_SPEC . $field . '.php';
+
+        return LoaderData::load($file);
+    }
+
+    /**
+     * Load main data.
+     *
+     * @param string $field
+     * @param string $locale
+     *
+     * @return array
+     *
+     * @throws InvalidArgumentException
+     */
+    static public function loadMainData($field, $locale)
+    {
+        if (!static::isSupportedMainData($field, $locale)) {
+            throw new InvalidArgumentException('Data for ' . $field . ' not supported.');
+        }
+
+        $file = static::getLocalePath($field, $locale);
+
+        return LoaderData::load($file);
+    }
+
+    /**
+     * Check supported main data.
+     *
+     * @param string $field
+     * @param string $locale
+     *
+     * @return boolean
+     */
+    static protected function isSupportedMainData($field, $locale)
+    {
+        if (!static::isSupportedLocale($locale)) {
+            return false;
+        }
+
+        if (!in_array($field, static::$mainData)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Check supported supplemental data.
+     *
+     * @param string $field
+     * @param string $locale
+     *
+     * @return boolean
+     */
+    static protected function isSupportedSupplementalData($field)
+    {
+        if (!in_array($field, static::$supplementalData)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Check supported locale.
+     *
+     * @param string $field
+     * @param string $locale
+     *
+     * @return boolean
+     */
+    static protected function isSupportedLocale($locale)
+    {
+        $data = \Locale::parseLocale($locale);
+
+        if (!in_array($locale, static::$locales) && !in_array($data['language'], static::$locales)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Get locale path.
+     *
+     * @param string $field
+     * @param string $locale
+     *
+     * @return string
+     */
+    static protected function getLocalePath($field, $locale)
+    {
+        $data = \Locale::parseLocale($locale);
+
+        $file1 = __DIR__ . D_SPEC . 'Data' . D_SPEC . $locale . D_SPEC . $field . '.php';
+        $file2 = __DIR__ . D_SPEC . 'Data' . D_SPEC . $data['language'] . D_SPEC . $field . '.php';
+
+        if (LoaderData::isReadable($file1)) {
+            return $file1;
+        }
+
+        return $file2;
+    }
+}
---
+++ tests/VhmisTest/I18n/Resource/LoaderTest.php
@@ -0,0 +1,114 @@
+<?php
+
+/**
+ * Vhmis Framework
+ *
+ * @link http://github.com/micti/VHMIS-Framework for git source repository
+ * @copyright Le Nhat Anh (http://lenhatanh.com)
+ * @license http://opensource.org/licenses/MIT MIT License
+ */
+
+namespace VhmisTest\I18n\Resource\Loader;
+
+use Vhmis\I18n\Resource\Loader;
+
+class LoaderTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @expectedException \Vhmis\Utils\Exception\InvalidArgumentException
+     */
+    public function testLoadMainDataWithNotSupportedLocale()
+    {
+        Loader::loadMainData('listPatterns', 'es-ES');
+    }
+
+    /**
+     * @expectedException \Vhmis\Utils\Exception\InvalidArgumentException
+     */
+    public function testLoadMainDataWithNotSupportedField()
+    {
+        Loader::loadMainData('ca', 'vi-VN');
+    }
+
+    public function testLoadMainData()
+    {
+        $result = [
+            'listPatterns' => [
+                'listPattern-type-standard' => [
+                    'start' => '{0}, {1}',
+                    'middle' => '{0}, {1}',
+                    'end' => '{0} và {1}',
+                    2 => '{0} và {1}',
+                ],
+                'listPattern-type-unit' => [
+                    'start' => '{0}, {1}',
+                    'middle' => '{0}, {1}',
+                    'end' => '{0}, {1}',
+                    2 => '{0}, {1}',
+                ],
+                'listPattern-type-unit-narrow' => [
+                    'start' => '{0}, {1}',
+                    'middle' => '{0}, {1}',
+                    'end' => '{0}, {1}',
+                    2 => '{0}, {1}',
+                ],
+                'listPattern-type-unit-short' => [
+                    'start' => '{0}, {1}',
+                    'middle' => '{0}, {1}',
+                    'end' => '{0}, {1}',
+                    2 => '{0}, {1}',
+                ],
+            ],
+        ];
+        $this->assertEquals($result, Loader::loadMainData('listPatterns', 'vi-VN'));
+    }
+
+    public function testLoadMainDataWithLang()
+    {
+        $result = [
+            'listPatterns' => [
+                'listPattern-type-standard' => [
+                    'start' => '{0}, {1}',
+                    'middle' => '{0}, {1}',
+                    'end' => '{0} và {1}',
+                    2 => '{0} và {1}',
+                ],
+                'listPattern-type-unit' => [
+                    'start' => '{0}, {1}',
+                    'middle' => '{0}, {1}',
+                    'end' => '{0}, {1}',
+                    2 => '{0}, {1}',
+                ],
+                'listPattern-type-unit-narrow' => [
+                    'start' => '{0}, {1}',
+                    'middle' => '{0}, {1}',
+                    'end' => '{0}, {1}',
+                    2 => '{0}, {1}',
+                ],
+                'listPattern-type-unit-short' => [
+                    'start' => '{0}, {1}',
+                    'middle' => '{0}, {1}',
+                    'end' => '{0}, {1}',
+                    2 => '{0}, {1}',
+                ],
+            ],
+        ];
+        $this->assertEquals($result, Loader::loadMainData('listPatterns', 'vi-KR'));
+    }
+
+    /**
+     * @expectedException \Vhmis\Utils\Exception\InvalidArgumentException
+     */
+    public function testLoadSupplementalDataWithNotSupportedField()
+    {
+        Loader::loadSupplementalData('af');
+    }
+
+    public function testLoadSupplementalData()
+    {
+        $data = Loader::loadSupplementalData('plurals');
+        $result = 'i = 0 or n = 1 @integer 0, 1 @decimal 0.0~1.0, 0.00~0.04';
+        $this->assertEquals($result, $data['am']['pluralRule-count-one']);
+    }
+}
